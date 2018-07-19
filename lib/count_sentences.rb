33c922dc625998026093d0f3ea3ef86ad7486200
Ramona M <r1mari@yahoo.com>
2018-07-19 12:03:11 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # self is true only when self ends with a "."
+    # else is false
+     self.end_with?(".")
+   
   end
 
   def question?
-
+self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
+    #should use self to refer to String
+    #1. split the string on all punctuation
+    complex_strings=[]
+self.split(/\s+/).length
 
   end
 end
